5f4acab746cc3e3fe8b34851b03b896bf9c10972
Tomas Hipolito <tlhipolito67@gmail.com>
2017-09-18 15:21:56 -0500

    Solution for the first exercise.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,70 @@
 public class HelloWorld {
+    //pvsm
+    //Entry point
     public static void main(String[] args) {
-        System.out.print("hello redwood");
+        //sout
+
+        //general syntax: type identifier;
+
+        //declaring and assigning
+
+        //3 types of integers
+        //in js it would be
+        //var byteNumber;
+        //var shortNumber;
+
+
+
+        //these are the basic types for java
+        //everything that starts with an uppercase is an object
+
+        byte byteNumber = 2;
+        short shortNumber = 0;
+        int intNumber = 1;
+        long longNumber = 10;
+        // floating point numbers
+        float floatNumber = 2.0f;
+        // Decimals will always be a double
+        double doubleNumber = 10.5;
+
+        char singleCharacter = 'l';
+
+        boolean booleanValue = true;
+        //This is a wrapper
+        //Boolean booleanValue = true
+
+        String name = "redwood";//always use double quotes
+
+
+        // const My_Constant = 4
+        final int My_Constant = 4;
+
+        System.out.println(3 + 4);
+        System.out.println(3 * 4);
+        float division = 3 / 4.0f;
+        System.out.println(3 - 4);
+        System.out.println(3 / 4);
+        System.out.println(3 % 4.0);
+        int x = 5;
+        System.out.println(x++);//5
+        System.out.println(++x);//7
+        System.out.println(x--);//7
+        System.out.println(--x);//5
+
+        //Casting
+        // It is only valid if the value on the right is smaller than the type on the left
+        double aDoubleNumber = 10;
+        System.out.println(aDoubleNumber);// from int to double
+
+        //explicit type casting
+        //type variable = (type) aBiggerTypeVariable
+        //when the thing on the right is bigger you need to convert it
+        int aIntergerNumber =(int) aDoubleNumber;
+
+
+
+        System.out.print("hello redwood!");
+
         System.out.println("hello world!");
     }
 }
---
+++ src/java_syntax.java
@@ -0,0 +1,46 @@
+public class java_syntax {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 13;
+        System.out.println("My favorite Number is "+ myFavoriteNumber);
+
+//        String myString = "Tomas";
+//        System.out.println(myString);
+//
+//        char myString = 'A';
+//        System.out.println(myString);
+
+//        String myString = "3.14159";
+//        System.out.println(myString);
+
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+        float myNumber = 123;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x2 = 5;
+//        System.out.println(++x2);
+//        System.out.println(x2);
+
+
+        int x = 4;
+         x += 5;
+        System.out.println(x);
+
+
+        int y = 4;
+         y *= x;
+        System.out.println(y);
+
+        x /= y;
+        y -=x;
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
+}
