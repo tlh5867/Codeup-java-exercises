aaefa1f59486ff5b003bf986c13381565e53847f
Tomas Hipolito <tlhipolito67@gmail.com>
2017-09-21 13:31:23 -0500

    completed the java exercise

---
+++ src/MethodExercise.java
@@ -0,0 +1,21 @@
+public class MethodExercise {
+    public static void main(String[] args) {
+        System.out.println(add(1,1));
+        System.out.println(sub(5,5));
+        System.out.println(multiply(5,5));
+        System.out.println(divide(5,5));
+    }
+
+    public static int add(int x, int y){
+        return x + y;
+    }
+    public static int sub(int x, int y){
+        return x - y;
+    }
+    public static int multiply(int x, int y){
+        return x * y;
+    }
+    public static int divide(int x, int y){
+        return x / y;
+    }}
+
---
+++ src/Method_notes.java
@@ -0,0 +1,16 @@
+public class Method_notes {
+    public static void main(String[] args) {
+        sayHello("Tomas ", 29);
+//        System.out.println(add(x:1,y:2))
+    }
+//    //each method is independent and will be outside of another method
+    public static void sayHello(String name,int age){
+        System.out.println("Hello " + name + "age " + age);
+//        System.out.println(add(x:5,y:6))
+//    }
+//
+//    public static int add(int x, int y){
+//        return x + y ;
+    }
+
+}
