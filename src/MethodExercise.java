3810c2a14932c17b01bcedae95d804b8a9ac348d
Tomas Hipolito <tlhipolito67@gmail.com>
2017-09-26 11:34:39 -0500

    completed the object oriented programming exercise

--- src/MethodExercise.java
+++ src/MethodExercise.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class MethodExercise {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println(add(1,1));
         System.out.println(sub(5,5));
         System.out.println(multiply(5,5));
         System.out.println(divide(5,5));
+        System.out.print("Enter a number between 1 and 10:");
+
     }
+
 
     public static int add(int x, int y){
         return x + y;
@@ -17,5 +23,41 @@ public class MethodExercise {
     }
     public static int divide(int x, int y){
         return x / y;
-    }}
+    }
+
+    public static int counter (int min, int max) {
+        System.out.println("Enter a Number between 1 and 10");
+        Scanner input = new Scanner(System.in);
+        int userInput = input.nextInt();
+
+        if (userInput < 1 || userInput > 10) {
+            userInput = counter(min, max);
+        } else {
+            return userInput;
+        }
+
+        return userInput;
+    }
+
+//    public static int factor (int min, int max) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter a number between 1 and 10:");
+//        int number = input.nextInt();
+//        input.nextLine();
+//
+////        for( i =1; i <= number,i++)
+//
+//    }}
+
+
+
+
+
+    }
+
+
+
+
+
+
 
