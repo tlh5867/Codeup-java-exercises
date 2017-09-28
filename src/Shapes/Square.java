3ebae2246c31206ac542eb04b82a0ae3f0331284
Tomas Hipolito <tlhipolito67@gmail.com>
2017-09-28 16:32:31 -0500

    -Created the quadrilateral class -crated the an abstract shape class -created a constructor in the quadrilateral class that accepts lenght and width inputs

--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -1,14 +1,3 @@
 package Shapes;
 
-public class Square extends Rectangle {
-    public Square(double side){
-        super(side,side);
-    }
-public double getPerimeter(){
-        return this.length*4;
 
-    }
-    public double getArea(){
-    return this.length*this.length;
-    }
-}
